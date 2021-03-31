4ed516acfc407f624c792985ef86fb37753f61d1
Subin Siby <mail@subinsb.com>
2021-04-01 03:39:15 +0530

    Trying to make vowels work aaaaaaa

--- nv.py
+++ nv.py
@@ -1,23 +1,37 @@
 import sqlite3
 vowels = {
     "a": ["അ", ""],
-    "aa": ["ആ", "ാ"]
+    "aa": ["ആ", "ാ"],
+    "i": ["ഇ", "ി"],
 }
 
 consonants = {
     "k": ["ക", "ഖ"],
     "m": ["മ", "ം"],
-    "l": ["ല", "ള"]
+    "l": ["ല", "ള"],
+    "L": ["ള"],
+    "v": ["വ"],
+    "t": ["റ്റ"],
+    "th": ["ത"],
 }
 
 chil = {
-    "m": ["ം", "മ"]
+    "m": ["ം", "മ"],
+    "l": ["ൽ", "ല"]
 }
 
 scheme = []
 
 
 def makeScheme():
+    for c, cList in vowels.items():
+        scheme.append({
+            "combo": c,
+            "i": cList,  # info
+            "weight": 0,
+            "type": "vowel"
+        })
+
     for c, cList in consonants.items():
         weight = 0
         for ci in cList:
@@ -43,13 +57,20 @@ def makeScheme():
 
 
 makeScheme()
+# print(scheme)
 
 
-def search(combo):
+def search(combo, first=False):
     r = []
     for s in scheme:
         if s["combo"] == combo:
-            r.append(s)
+            t = s
+            if s["type"] == "vowel":
+                if first:
+                    t["i"] = t["i"][0]
+                else:
+                    t["i"] = t["i"][1]
+            r.append(t)
     return r
 
 
@@ -67,18 +88,20 @@ def make(word):
 
         if i == len(word) - 1 and len(sequence) == 1:
             # last char
-            match = search(ch)
-            if len(match) == 0:
-                add(ch)
+            if ch in chil:
+                add(chil[ch][0])
             else:
-                add(match)
+                match = search(ch, i == 0)
+                if len(match) == 0:
+                    add(ch)
+                else:
+                    add(match)
         elif len(search(sequence)) == 0:
-            print(sequence)
             if len(sequence) == 1:
                 add(sequence)
             else:
                 # found end, backtrack
-                add(search(sequence[0:-1]))
+                add(search(sequence[0:-1], i == 0))
                 i -= 1
             sequence = ""
 
@@ -97,22 +120,27 @@ def flatten(tokens):
             # t is possiblities
             if first:
                 for possibility in t:
-                    results.append(possibility["i"])
+                    results.append([possibility["i"], possibility["weight"]])
                 first = False
             else:
                 for j in range(len(results)):
-                    till = results[j]
-                    results[j] = results[j] + t[0]["i"]
+                    till = results[j][0]
+                    tillWeight = results[j][1]
+
+                    results[j][0] += t[0]["i"]
+                    results[j][1] += t[0]["weight"]
+
                     for k in range(1, len(t)):
                         till += t[k]["i"]
-                        results.append(till)
+                        tillWeight += t[k]["weight"]
+                        results.append([till, tillWeight])
         else:
             if first:
-                results.append([t])
+                results.append([t, 0])
                 first = False
             else:
                 for j in range(len(results)):
-                    results[j] += t
+                    results[j][0] += t
         i += 1
 
     return results
@@ -122,4 +150,4 @@ def transliterate(word):
     return flatten(make(word))
 
 
-print(transliterate("kamalam"))
+print(transliterate("kaaaaaaaaa"))
